b41427959ae49fb587b19d873d6aff1d7daedf20
Yoselyn Cántaro <yoselynespinoza27@gmail.com>
2024-10-18 19:29:44 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/pe/com/cibertec/Lp2_spring/Lp2SpringApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.Lp2_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/cibertec/Lp2_spring/Lp2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.Lp2_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
